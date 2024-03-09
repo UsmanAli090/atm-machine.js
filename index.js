f43935326dc7a87bbf4f7585dce3769a767b955f
UsmanAli090 <157049066+UsmanAli090@users.noreply.github.com>
2024-03-09 20:36:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const prize = prompt ('ENTER THE AMOUNT VALUE YOU WANT TO WITHDRAW')
+const amount = 50000;
+switch (amount) {
+    case 50000:
+        document.write('Thanks for the Withdraw')
+        
+        break;
+
+        case 40000:
+            document.write('Thanks for the Withdraw')
+        
+        break;
+
+        case 60000:
+            document.write('WITHDRAW LIMIT IS OVER')
+        
+        break;
+
+
+        case 70000:
+        document.write('WITHDRAW LIMIT IS OVER')
+        break;
+
+
+        case 80000:
+        document.write('WITHDRAW LIMIT IS OVER')
+        break;
+
+        case 90000:
+        document.write('WITHDRAW LIMIT IS OVER')
+        break;
+
+        case 100000:
+        document.write('THIS AMOUNT IS INSUFFICIENT')
+        break;
+
+        
+
+
+
+    default:
+        document.write('ENTER VALUE OF YOUR AMOUNT')
+        break;
+}
